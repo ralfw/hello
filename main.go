c6fdfe35fab5cadc46140d2344507c7616e3066e
Florian Boehmak <florian@fnbk.cc>
2018-05-03 17:47:34 +0200

    retrigger

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-		fmt.Fprintf(w, "Hello World")
+		fmt.Fprintf(w, "Hello awesome World")
 	})
 
 	http.ListenAndServe(":80", nil)
