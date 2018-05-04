b66c8ae8024957782171ac5da97346ed97218d85
Florian Boehmak <florian@fnbk.cc>
2018-05-04 11:40:36 +0200

    trigger

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-		fmt.Fprintf(w, "Hello awesome World")
+		fmt.Fprintf(w, "Hello pretty World")
 	})
 
 	http.ListenAndServe(":80", nil)
