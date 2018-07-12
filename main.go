679085bf8e13a617d244ad8db5574ef277f075db
Ralf Westphal <ralfw@users.noreply.github.com>
2018-07-12 10:57:55 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello ralf World")
+		fmt.Fprintf(w, "Hello, World! This is really working!")
 	})
 
 	http.ListenAndServe(":80", nil)
